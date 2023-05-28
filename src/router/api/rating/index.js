22ed8b2888de2524bb512c1678f03331bb5e9a78
kalmanbendeguz <61018403+kalmanbendeguz@users.noreply.github.com>
2023-05-28 07:44:20 +0200

    upload progress.xlsx

--- src/router/api/rating/index.js
+++ src/router/api/rating/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const create = require('../../../controllers/rating/create')
 //const find = require('../../../controllers/rating/find')
 //const update = require('../../../controllers/rating/update')
-//const remove = require('../../../controllers/rating/remove')
+const remove = require('../../../controllers/rating/remove')
 
 router.post('/', async (req, res, _) => {
     const result = await create(req.body, req.user)
@@ -20,11 +20,11 @@ router.post('/', async (req, res, _) => {
 //    const result = await update({ query: req.query, body: req.body }, req.user)
 //    return res.status(result.code).json(result.data)
 //})
-//
-//router.delete('/', async (req, res, _) => {
-//    const result = await remove(req.query, req.user)
-//    return res.status(result.code).json(result.data)
-//})
+
+router.delete('/', async (req, res, _) => {
+    const result = await remove(req.query, req.user)
+    return res.status(result.code).json(result.data)
+})
 
 module.exports = router
 
