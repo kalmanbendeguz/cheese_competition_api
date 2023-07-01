fa589fd7d2d96ac6b261e273ae7b1d6c517e42b7
kalmanbendeguz <61018403+kalmanbendeguz@users.noreply.github.com>
2023-07-01 15:20:15 +0200

    find allowed role validator

--- src/validators/requests/api/allowed_role/find.js
+++ src/validators/requests/api/allowed_role/find.js
@@ -1,16 +1,8 @@
 const Joi = require('joi')
 
 const query_validator = Joi.object({
-    filter: Joi.object(/*{
-        _id: Joi.any().optional(),
-        user_id: Joi.any().optional(),
-        restore_id: Joi.any().optional(),
-        expiring_started: Joi.any().optional(),
-    }*/).optional(),
-    projection: Joi.object()
-        .pattern(Joi.string(), Joi.valid(1).required())
-        .min(1)
-        .required(),
+    filter: Joi.object().optional(),
+    projection: Joi.object().pattern(Joi.string(), Joi.valid(0, 1)).min(1).required(),
     options: Joi.object({
         limit: Joi.number().integer().positive().optional(),
         skip: Joi.number().integer().min(0).optional(),
