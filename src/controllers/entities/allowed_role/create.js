601474b6094cecb624c9f89a6229b5e77a8f80c8
kalmanbendeguz <61018403+kalmanbendeguz@users.noreply.github.com>
2023-07-04 11:33:45 +0200

    create allowed_role controller

--- src/controllers/entities/allowed_role/create.js
+++ src/controllers/entities/allowed_role/create.js
@@ -1,8 +1,6 @@
-// SERVER, ORGANIZER
-module.exports = async (body, user) => {
-    const Allowed_Role_Model = require('../../../models/Allowed_Role')
+const create = async (body, user, parent_session) => {
 
-    // 1. validate body
+    // 1. Validate body
     const create_allowed_role_validator = require('../../../validators/requests/api/allowed_role/create')
     try {
         await create_allowed_role_validator.validateAsync(body)
@@ -10,38 +8,33 @@ module.exports = async (body, user) => {
         return { code: 400, data: err.details }
     }
 
-    // 2. arrayize
+    // 2. Arrayize
     body = Array.isArray(body) ? body : [body]
 
-    // 3. authorize {body, user}
-    const authorizer = require('../../../authorizers/allowed_role')
-    const authorizer_results = body.map((allowed_role) =>
-        authorizer(allowed_role, 'create', user)
-    )
-    const violation = authorizer_results.find((result) => !result.authorized)
-    if (violation) {
-        return { code: 403, data: violation.message }
+    // 3. Authorize create
+    const authorizer = require('../../../authorizers/entities/allowed_role')
+    try {
+        body = body.map((allowed_role) => authorizer(allowed_role, 'create', user))
+    } catch (reason) {
+        return {
+            code: 403,
+            data: reason
+        }
     }
 
-    // 4. check_dependencies : will the database be consistent if i create this document?
-    for (const allowed_role of body) {
-        //email should be unique
-        if (await Allowed_Role_Model.exists({ email: allowed_role.email }))
-            return {
-                code: 409,
-                data: 'allowed_role_with_email_already_exists',
-            }
-    }
+    // 4. Start session and transaction if they don't exist
+    const Allowed_Role_Model = require('../../../models/Allowed_Role')
+    const session = parent_session ?? await Allowed_Role_Model.startSession()
+    if (!session.inTransaction()) session.startTransaction()
 
-    // 5. prepare
-    const _allowed_roles = body
+    // 5. Create locally
+    const _allowed_roles = body.map((allowed_role) => ({
+        email: allowed_role.email,
+        allowed_roles: allowed_role.allowed_roles
+    }))
+    const allowed_roles = _allowed_roles.map((allowed_role) => new Allowed_Role_Model(allowed_role))
 
-    // 6. create
-    const allowed_roles = _allowed_roles.map(
-        (allowed_role) => new Allowed_Role_Model(allowed_role)
-    )
-
-    // 7. validate_documents
+    // 6. Validate created documents
     const allowed_role_validator = require('../../../validators/schemas/Allowed_Role')
     try {
         const validator_promises = allowed_roles.map((allowed_role) =>
@@ -49,27 +42,61 @@ module.exports = async (body, user) => {
         )
         await Promise.all(validator_promises)
     } catch (err) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.abortTransaction()
+            await session.endSession()
+        }
         return { code: 400, data: err.details }
     }
 
-    // 8. update_dependents
-    // dont add roles to existing users! bc they might have to provide additional data to be able to have a role.
-    // so if a [competitor] exists and we add the judge role: there may be judge specific fields on User.
-    // (btw at this time there are none...)
-    // the user will add the role for himself.
-    // or if possible, give them the role? well, dont. because at registration, he wont get the allowed roles, bc.
-    // he registers for a single role on the ui. so  if we dont give it to him on registration, then we should neither give it to them here.
-    // he will have to ask for it.
+    // 7. Check dependencies: Ask all dependencies if this creation is possible.
+    // Allowed_Role has no dependencies.
 
-    // 9. save
-    const saver_promises = allowed_roles.map((allowed_role) =>
-        allowed_role.save()
-    )
-    await Promise.all(saver_promises)
+    // 8. Check collection integrity
+    // email should be unique
+    const new_emails = allowed_roles.map(allowed_role => allowed_role.email)
+    const unique_new_emails = [...new Set(new_emails)]
+    if (new_emails.length !== unique_new_emails.length) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.abortTransaction()
+            await session.endSession()
+        } return {
+            code: 409,
+            data: 'provided_allowed_role_emails_are_not_unique',
+        }
+    }
+    if (await Allowed_Role_Model.exists({
+        email: { $in: unique_new_emails },
+    }, { session: session })
+    ) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.abortTransaction()
+            await session.endSession()
+        } return {
+            code: 409,
+            data: 'at_least_one_provided_allowed_role_email_already_exists_in_db',
+        }
+    }
 
-    // 10. reply
+    // 9. Save created documents
+    await Allowed_Role_Model.bulkSave(allowed_roles, { session: session })
+
+    // 10. Update dependents
+    // Nothing needs to be updated.
+    // Don't add roles to existing users, because they might have to provide additional data to be able to have that role.
+    // The user will add the role for himself.
+
+    // 11. Commit transaction and end session.
+    if (!parent_session) {
+        if (session.inTransaction()) await session.commitTransaction()
+        await session.endSession()
+    }
+
+    // 12. Reply
     return {
         code: 201,
-        data: undefined, // TODO, check if it works if i leave it out, etc.
+        data: undefined,
     }
 }
+
+module.exports = create
