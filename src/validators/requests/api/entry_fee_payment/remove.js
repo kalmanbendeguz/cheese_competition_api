dc0c55d4fb2ba7296f5b24dc8ca7e36217af0ae8
kalmanbendeguz <61018403+kalmanbendeguz@users.noreply.github.com>
2023-07-04 09:48:09 +0200

    remove entry fee payment validator

--- src/validators/requests/api/entry_fee_payment/remove.js
+++ src/validators/requests/api/entry_fee_payment/remove.js
@@ -3,8 +3,9 @@ const Joi = require('joi')
 const query_validator = Joi.object({
     _id: Joi.any().optional(),
     product_ids: Joi.any().optional(),
-    pending: Joi.any().optional(),
     barion_payment_id: Joi.any().optional(),
+    pending: Joi.any().optional(),
+    expiring_started: Joi.any().optional(),
     barion_transaction_id: Joi.any().optional(),
     amount: Joi.any().optional(),
     currency: Joi.any().optional(),
