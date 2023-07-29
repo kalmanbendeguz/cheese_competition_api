12c17eb1b3e3e2ebf19f6cfe5dd2f2b967f47802
kalmanbendeguz <61018403+kalmanbendeguz@users.noreply.github.com>
2023-07-29 19:26:35 +0200

    user remove controller

--- src/controllers/entities/user/remove.js
+++ src/controllers/entities/user/remove.js
@@ -1,11 +1,6 @@
-// ONLY SERVER
-module.exports = async (query, user) => {
-    const User_Model = require('../../../models/User')
-    const remove_active_password_reset = require('../active_password_reset/remove')
-    const remove_product = require('../product/remove')
-    const remove_rating = require('../rating/remove')
+const remove = async (query, user, parent_session) => {
 
-    // 1. validate query
+    // 1. Validate query
     const remove_user_validator = require('../../../validators/requests/api/user/remove')
     try {
         await remove_user_validator.validateAsync(query)
@@ -13,49 +8,159 @@ module.exports = async (query, user) => {
         return { code: 400, data: err.details }
     }
 
-    // 2. authorize {query, user}
-    const authorizer = require('../../../authorizers/user')
-    const authorizer_result = authorizer(query, 'remove', user)
-    if (!authorizer_result.authorized) {
-        return { code: 403, data: authorizer_result.message }
+    // 2. Authorize remove
+    const authorizer = require('../../../authorizers/entities/user')
+    try {
+        query = authorizer(query, 'remove', user)
+    } catch (reason) {
+        return {
+            code: 403,
+            data: reason
+        }
     }
 
-    // 3. prepare find: produce 'filter' completely. if only server, then no changes should happen(?)
-    const filter = query
+    // 3. Start session and transaction if they don't exist
+    const User_Model = require('../../../models/User')
+    const session = parent_session ?? await User_Model.startSession()
+    if (!session.inTransaction()) session.startTransaction()
 
-    // 4. find
-    const users = await User_Model.find(filter)
-    if (users.length === 0)
+    // 4. Find
+    const filter = query
+    const users = await User_Model.find(filter, null, { session: session })
+    if (users.length === 0) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.commitTransaction()
+            await session.endSession()
+        }
         return {
-            code: 404,
+            code: 200,
             data: 'no_documents_found_to_remove',
         }
-
-    // 5. check dependents
-    // nothing needs to be done here.
-
-    // 6. update dependents
-    for (const u of users) {
-        // remove active_password_resets
-        await remove_active_password_reset(
-            { user_id: u._id },
-            { role: 'SERVER' }
-        )
-        // remove products
-        await remove_product({ competitor_id: u._id }, { role: 'SERVER' })
-        // remove ratings
-        await remove_rating({ judge_id: u._id }, { role: 'SERVER' })
     }
 
-    // 7. remove
-    const ids_to_delete = users.map((u) => u._id)
+    // 5. Validate documents
+    const user_validator = require('../../../validators/schemas/User')
+    try {
+        const validator_promises = users.map(
+            (u) => user_validator.validateAsync(u)
+        )
+        await Promise.all(validator_promises)
+    } catch (err) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.abortTransaction()
+            await session.endSession()
+        }
+        return { code: 500, data: err.details }
+    }
+
+    // 6. Check dependencies: Ask all dependencies if this remove is possible.
+    const dependencies = ['allowed_role', 'competition']
+    const dependency_approvers = dependencies.map(dependency => require(`../${dependency}/approve_dependent_mutation/user`))
+
+    const dependency_approver_promises = []
+    for (const dependency_approver of dependency_approvers) {
+        dependency_approver_promises.push(dependency_approver(users.map(u => ({ old: u, new: null })), user, session))
+    }
+    const dependency_approver_results = await Promise.all(dependency_approver_promises)
+
+    const unapproved = dependency_approver_results.find(dependency_approver_result => !dependency_approver_result.approved)
+    if (unapproved) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.abortTransaction()
+            await session.endSession()
+        }
+        return {
+            code: 403,
+            data: unapproved.reason
+        }
+    }
+
+    // 7. Check collection integrity
+    // You cannot change the number of organizers from N>0 to N=0
+    const organizers_in_total = await User_Model.find(
+        { roles: { $in: ['organizer'] } },
+        null,
+        { session: session }
+    )
+    const organizers_to_remove = users.filter(u => u.roles.includes('organizer'))
+    if (organizers_in_total.length === organizers_to_remove.length) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.abortTransaction()
+            await session.endSession()
+        }
+        return {
+            code: 403,
+            data: 'it_is_not_allowed_to_remove_the_last_organizer',
+        }
+    }
+
+    // 8. Remove documents
+    const ids_to_delete = users.map((u) => u._id.toString())
     await User_Model.deleteMany({
         _id: { $in: ids_to_delete },
+    }, {
+        session: session
     })
 
-    // 8. reply
+    // 9. Update dependents
+    // Dependents are: Active_Password_Reset, Rating, Product
+
+    const update_dependent_promises = []
+
+    // If we remove a User, we should remove all Active_Password_Resets associated with it.
+    const remove_active_password_reset = require('../active_password_reset/remove')
+    update_dependent_promises.push(
+        remove_active_password_reset(
+            { user_id: { $in: ids_to_delete } },
+            user,
+            session
+        )
+    )
+
+    // If we remove a User, we should remove all Ratings associated with it.
+    const remove_rating = require('../rating/remove')
+    update_dependent_promises.push(
+        remove_rating(
+            { judge_id: { $in: ids_to_delete } },
+            user,
+            session
+        )
+    )
+
+    // If we remove a User, we should remove all Products associated with it.
+    const remove_product = require('../product/remove')
+    update_dependent_promises.push(
+        remove_product(
+            { competitor_id: { $in: ids_to_delete } },
+            user,
+            session
+        )
+    )
+
+    const update_dependent_results = await Promise.all(update_dependent_promises)
+    const failed_operation = update_dependent_results.find(result =>
+        !(typeof result.code === 'number' && result.code >= 200 && result.code <= 299)
+    )
+
+    if (failed_operation) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.abortTransaction()
+            await session.endSession()
+        }
+        return failed_operation
+    }
+
+    // 10. Commit transaction and end session
+    if (!parent_session) {
+        if (session.inTransaction()) await session.commitTransaction()
+        await session.endSession()
+    }
+
+    // 11. Reply
     return {
         code: 200,
         data: undefined,
     }
 }
+
+module.exports = remove
