44730cf6b569a9b0fe108beae4f0176079b2215a
kalmanbendeguz <61018403+kalmanbendeguz@users.noreply.github.com>
2023-08-02 08:56:31 +0200

    Rating_Picture schema validator

--- src/validators/schemas/Rating_Picture.js
+++ src/validators/schemas/Rating_Picture.js
@@ -1,5 +1,10 @@
 const Joi = require('joi')
+const { mongoose: { Types: { ObjectId }, }, } = require('mongoose')
+const File_Validator = require('./File')
 
-const rating_picture_validator = Joi.object().optional().unknown(true)
+const rating_picture_validator = Joi.object({
+    rating_id: Joi.object().instance(ObjectId).required(),
+    picture: File_Validator.required(),
+}).unknown(true)
 
 module.exports = rating_picture_validator
