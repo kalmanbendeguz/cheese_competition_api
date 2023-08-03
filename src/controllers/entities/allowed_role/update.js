6aa438c614a3d5bd37664b1f64dd911faa6b9e68
kalmanbendeguz <61018403+kalmanbendeguz@users.noreply.github.com>
2023-08-03 20:42:39 +0200

    allowed_role update controller

--- src/controllers/entities/allowed_role/update.js
+++ src/controllers/entities/allowed_role/update.js
@@ -1,8 +1,6 @@
-// ONLY SERVER
-module.exports = async (data, user) => {
-    const update_user = require('../user/update')
+const update = async (data, user, parent_session) => {
 
-    // 1. validate data
+    // 1. Validate data
     const update_allowed_role_validator = require('../../../validators/requests/api/allowed_role/update')
     try {
         await update_allowed_role_validator.validateAsync(data)
@@ -10,128 +8,153 @@ module.exports = async (data, user) => {
         return { code: 400, data: err.details }
     }
 
-    // 2. authorize updatable
-    const authorizer = require('../../../authorizers/allowed_role')
-    const updatable_authorizer_result = authorizer(
-        data.query,
-        'updatable',
-        user
-    )
-    if (!updatable_authorizer_result.authorized) {
-        return { code: 403, data: updatable_authorizer_result.message }
-    }
-
-    // 3. authorize update
-    const update_authorizer_result = authorizer(data.body, 'update', user)
-    if (!update_authorizer_result.authorized) {
-        return { code: 403, data: update_authorizer_result.message }
-    }
-
-    // 3. prepare find
-    const filter = data.query
-
-    // 4. find
-    const Allowed_Role_Model = require('../../../models/Allowed_Role')
-    const allowed_roles = await Allowed_Role_Model.find(filter)
-    if (allowed_roles.length === 0)
-        return {
-            code: 404,
-            data: 'no_documents_found_to_update',
-        }
-
-    // 5. check dependencies : will the database be consistent if i make this update?
-    // an organizer cannot remove his organizer role
-    if (
-        user.role === 'organizer' &&
-        ((!Array.isArray(data.body.allowed_roles) &&
-            data.body.allowed_roles.split(' ').includes('-organizer')) ||
-            (Array.isArray(data.body.allowed_roles) &&
-                !data.body.includes('organizer')))
-    )
+    // 2. Authorize updatable
+    const authorizer = require('../../../authorizers/entities/allowed_role')
+    try {
+        data.query = authorizer(data.query, 'updatable', user)
+    } catch (reason) {
         return {
             code: 403,
-            data: 'an_organizer_can_not_remove_his_own_organizer_role',
+            data: reason
         }
-
-    // 6. prepare_update
-    const all_roles = ['judge', 'organizer', 'receiver']
-    let roles_to_add = []
-    let roles_to_remove = []
-    if (Array.isArray(data.body.allowed_roles)) {
-        // it is an array
-        roles_to_add = data.body.allowed_roles
-        roles_to_remove = all_roles.filter(
-            (role) => !roles_to_add.includes(role)
-        )
-    } else {
-        // it is a string
-        const role_actions = data.body.allowed_roles.split(' ')
-        roles_to_add = role_actions.filter((action) => action[0] === '+')
-        roles_to_remove = role_actions.filter((action) => action[0] === '-')
     }
-    let update = {}
-    const remove = []
+    const filter = data.query
 
-    // 7. update
+    // 3. Authorize update
+    try {
+        data.body = authorizer(data.body, 'update', user)
+    } catch (reason) {
+        return {
+            code: 403,
+            data: reason
+        }
+    }
+    const update = data.body
+
+    // 4. Start session and transaction if they don't exist
+    const Allowed_Role_Model = require('../../../models/Allowed_Role')
+    const session = parent_session ?? await Allowed_Role_Model.startSession()
+    if (!session.inTransaction()) session.startTransaction()
+
+    // 5. Find
+    const allowed_roles = (await Allowed_Role_Model.find(
+        filter,
+        null,
+        { session: session }
+    )).map(allowed_role => ({ old: structuredClone(allowed_role), new: allowed_role }))
+    if (allowed_roles.length === 0) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.commitTransaction()
+            await session.endSession()
+        }
+        return {
+            code: 200,
+            data: 'no_documents_found_to_update',
+        }
+    }
+
+    // 6. Update locally
+    // We need to go in a topological order.
+    // For every field, we deal with the field and its dependencies, but not its dependents.
     for (const allowed_role of allowed_roles) {
-        const current_allowed_roles_array = allowed_role.allowed_roles
-        const modified_allowed_roles_array = [
-            ...new Set(
-                current_allowed_roles_array
-                    .filter((ar) => !roles_to_remove.includes(ar))
-                    .concat(roles_to_add)
-            ),
-        ]
+        const current_update = structuredClone(update)
+        const current_remove = []
 
-        update = {
-            allowed_roles: modified_allowed_roles_array,
+        // email cannot be updated
+
+        // allowed_roles: if it is a string, then convert modifier string to an actual new allowed_roles array
+        if ('allowed_roles' in current_update && typeof current_update.allowed_roles === 'string') {
+            const allowed_role_modifiers = current_update.allowed_roles.split(' ')
+            const allowed_roles_to_add = allowed_role_modifiers.filter(allowed_role_modifier => allowed_role_modifier[0] === '+').map(allowed_role_modifier => allowed_role_modifier.substring(1))
+            const allowed_roles_to_remove = allowed_role_modifiers.filter(allowed_role_modifier => allowed_role_modifier[0] === '-').map(allowed_role_modifier => allowed_role_modifier.substring(1))
+            current_update.allowed_roles = allowed_role.old.allowed_roles.filter(allowed_role => !allowed_roles_to_remove.includes(allowed_role)).concat(allowed_roles_to_add)
         }
 
-        allowed_role.set(update)
-
-        for (const key of remove) {
-            allowed_role[key] = undefined
+        allowed_role.new.set(current_update)
+        for (const key of current_remove) {
+            allowed_role.new[key] = undefined
         }
     }
 
-    // 8. validate documents
+    // 7. Validate new documents
     const allowed_role_validator = require('../../../validators/schemas/Allowed_Role')
     try {
         const validator_promises = allowed_roles.map((allowed_role) =>
-            allowed_role_validator.validateAsync(allowed_role)
+            allowed_role_validator.validateAsync(allowed_role.new)
         )
         await Promise.all(validator_promises)
     } catch (err) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.abortTransaction()
+            await session.endSession()
+        }
         return { code: 500, data: err.details }
     }
 
-    // 9. update dependents
-    // if we added an allowed role, nothing needs to be done.
-    // if we remove an allowed role, we should remove it from the user as well.
-    const update_user_promises = allowed_roles.map((allowed_role) =>
-        update_user(
+    // 8. Check dependencies: Ask all dependencies if this update is possible.
+    // Allowed_Role has no dependencies.
+
+    // 9. Check collection integrity
+    // Nothing needs to be checked.
+
+    // 10. Save updated documents
+    await Allowed_Role_Model.bulkSave(allowed_roles.map(allowed_role => allowed_role.new), { session: session })
+
+    // 11. Update dependents
+    // Only dependent: User
+    const update_user = require('../user/update')
+
+    const update_dependent_promises = []
+
+    for (const allowed_role of allowed_roles) {
+        // Don't add roles to existing users, because they might have to provide additional data to be able to have that role.
+        // The user will add the role for himself.
+        // But we still need to remove the removed roles.
+        const removed_roles = allowed_role.old.allowed_roles.filter(a_r => !allowed_role.new.allowed_roles.includes(a_r))
+        const removed_role_modifier_strings = removed_roles.map(removed_role => `-${removed_role}`)
+
+        if (removed_role_modifier_strings.length === 0) continue
+
+        const allowed_role_modifier_string = `${removed_role_modifier_strings.join(' ')}`
+
+        update_dependent_promises.push(update_user(
             {
-                query: { email: allowed_role.email },
-                body: {
-                    roles: roles_to_remove.map((ar) => `-${ar}`).join(' '),
+                query: {
+                    email: allowed_role.new.email
                 },
+                body: {
+                    roles: allowed_role_modifier_string
+                }
             },
-            {
-                role: 'SERVER',
-            }
-        )
-    )
-    await Promise.all(update_user_promises)
+            user,
+            session
+        ))
+    }
 
-    // 10. save
-    const saver_promises = allowed_roles.map((allowed_role) =>
-        allowed_role.save()
+    const update_dependent_results = await Promise.all(update_dependent_promises)
+    const failed_operation = update_dependent_results.find(result =>
+        !(typeof result.code === 'number' && result.code >= 200 && result.code <= 299)
     )
-    await Promise.all(saver_promises)
 
-    // 11. reply
+    if (failed_operation) {
+        if (!parent_session) {
+            if (session.inTransaction()) await session.abortTransaction()
+            await session.endSession()
+        }
+        return failed_operation
+    }
+
+    // 12. Commit transaction and end session
+    if (!parent_session) {
+        if (session.inTransaction()) await session.commitTransaction()
+        await session.endSession()
+    }
+
+    // 13. Reply
     return {
         code: 200,
         data: undefined,
     }
 }
+
+module.exports = update
