ca01f808939be1f33d0fb6d31b89408b83e46df8
kalmanbendeguz <61018403+kalmanbendeguz@users.noreply.github.com>
2023-08-14 15:41:59 +0200

    registration controller

--- src/controllers/actions/registration/index.js
+++ src/controllers/actions/registration/index.js
@@ -1,6 +1,6 @@
 const registration = async (req, res, next) => {
-    // validate request, but only the parts that create_user doesnt validate, like Password
-    // 1. Validate ******
+
+    // 1. Validate request
     const registration_validator = require('../../../validators/requests/api/registration')
     try {
         await registration_validator.validateAsync(req)
@@ -8,41 +8,62 @@ const registration = async (req, res, next) => {
         return res.status(400).json(`registration_validation_error: ${err.details}`)
     }
 
-    // start session and transaction
+    // 2. Authorize action
+    const authorize_action = require('../../../helpers/authorize_action')
+    try {
+        authorize_action('registration', req.user)
+    } catch (reason) {
+        return res.status(403).json(reason)
+    }
+
+    // 3. Start session and transaction
     const db = require('../../../config/db')
     const session = await db.startSession()
     if (!session.inTransaction()) session.startTransaction()
 
-    // hash password
+    // 4. Action
+
+    // Hash password
     const bcrypt = require('bcrypt')
     const salt = await bcrypt.genSalt()
-    const hashed_password = await bcrypt.hash(req.body.plain_password, salt) // PLAIN_PASSWORD!
+    const hashed_password = await bcrypt.hash(req.body.plain_password, salt)
     req.body.hashed_password = hashed_password
     delete req.body.plain_password
 
-    console.log(req.body)
-
-    // create user
+    // Create user
     const create_user = require('../../entities/user/create')
     const create_user_result = await create_user(req.body, { role: 'SERVER' }, session)
-    if (create_user_result.code !== 201) {
+    if (!(typeof create_user_result.code === 'number' && create_user_result.code >= 200 && create_user_result.code <= 299)) {
         if (session.inTransaction()) await session.abortTransaction()
         await session.endSession()
         return res.status(create_user_result.code).json(create_user_result.data)
     }
-    // find user
+
+    // Find created user
     const find_user = require('../../entities/user/find')
     const find_user_result = await find_user(
-        { filter: { email: req.body.email }, projection: { confirm_registration_id: 1 } },
+        {
+            filter: { email: req.body.email },
+            projection: {
+                email: 1,
+                confirm_registration_id: 1
+            }
+        },
         { role: 'SERVER' },
         session
     )
     const created_user = find_user_result.data[0]
 
-    console.log(find_user_result)
-    // send email
-    const iso_639_1_language = req?.cookies?.language ?? process.env.DEFAULT_LANGUAGE ?? 'en' // TODO: DETECT THIS AUTOMATICALLY!
-    const recipient = req.body.email
+    // Send confirm registration email
+    const iso_639_1_language = req?.cookies?.language ?? process.env.DEFAULT_LANGUAGE ?? 'en'
+    const valid_iso_639_1_languages = require('../../../static/valid_iso_639_1_languages.json')
+    if (!valid_iso_639_1_languages.includes(iso_639_1_language)) {
+        if (session.inTransaction()) await session.abortTransaction()
+        await session.endSession()
+        return next('invalid_language_cookie')
+    }
+
+    const recipient = created_user.email
     const get_string = require('../../../helpers/get_string')
     const title = get_string('confirm_registration_email_title', iso_639_1_language, null)
     const server_url = process.env.NODE_ENV === 'production' ?
@@ -62,13 +83,12 @@ const registration = async (req, res, next) => {
         return next(error)
     }
 
-    // end session and transaction
-    // 11. Commit transaction and end session.
+    // 5. Commit transaction and end session
     if (session.inTransaction()) await session.commitTransaction()
     await session.endSession()
 
-    // reply
-    return res.status(200).json(`successful_registration`)
+    // 6. Reply
+    return res.status(200).json(`registration_successful_please_confirm_via_email_link`)
 }
 
 module.exports = registration
