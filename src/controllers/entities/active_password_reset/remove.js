1801b3c1f1d69e7ed383998700bfdb0200faee24
kalmanbendeguz <61018403+kalmanbendeguz@users.noreply.github.com>
2023-09-04 05:00:49 +0200

    all controllers of active_password_reset

--- src/controllers/entities/active_password_reset/remove.js
+++ src/controllers/entities/active_password_reset/remove.js
@@ -1,107 +1,25 @@
-const remove = async (query, user, parent_session) => {
+const remove = async (query, actor, session) => {
 
-    // 1. Validate query
-    const remove_active_password_reset_validator = require('../../../validators/requests/api/active_password_reset/remove')
-    try {
-        await remove_active_password_reset_validator.validateAsync(query)
-    } catch (err) {
-        return { code: 400, data: err.details }
-    }
-
-    // 2. Authorize remove
-    const authorizer = require('../../../authorizers/entities/active_password_reset')
-    try {
-        query = authorizer(query, 'remove', user)
-    } catch (reason) {
-        return {
-            code: 403,
-            data: reason
-        }
-    }
-
-    // 3. Start session and transaction if they don't exist
+    // 1. Find
     const Active_Password_Reset_Model = require('../../../models/Active_Password_Reset')
-    const session = parent_session ?? await Active_Password_Reset_Model.startSession()
-    if (!session.inTransaction()) session.startTransaction()
+    const active_password_resets = await Active_Password_Reset_Model.find(query.filter, null, { session: session })
 
-    // 4. Find
-    const filter = query
-    const active_password_resets = await Active_Password_Reset_Model.find(filter, null, { session: session })
-    if (active_password_resets.length === 0) {
-        if (!parent_session) {
-            if (session.inTransaction()) await session.commitTransaction()
-            await session.endSession()
-        }
-        return {
-            code: 200,
-            data: 'no_documents_found_to_remove',
-        }
+    for (const active_password_reset of active_password_resets) {
+        // 2. Check dependencies
+        // Dependencies: [User]
+        // Nothing needs to be checked.
+
+        // 3. Remove
+        await active_password_reset.deleteOne({ session: session })
+
+        // 4. Update dependents
+        // There are no dependents of Active_Password_Reset.
+
     }
 
-    // 5. Validate documents
-    const active_password_reset_validator = require('../../../validators/schemas/Active_Password_Reset')
-    try {
-        const validator_promises = active_password_resets.map(
-            (active_password_reset) =>
-                active_password_reset_validator.validateAsync(
-                    active_password_reset
-                )
-        )
-        await Promise.all(validator_promises)
-    } catch (err) {
-        if (!parent_session) {
-            if (session.inTransaction()) await session.abortTransaction()
-            await session.endSession()
-        }
-        return { code: 500, data: err.details }
-    }
-
-    // 6. Check dependencies: Ask all dependencies if this remove is possible.
-    const dependencies = ['user']
-    const dependency_approvers = dependencies.map(dependency => require(`../${dependency}/approve_dependent_mutation/active_password_reset`))
-
-    const dependency_approver_promises = []
-    for (const dependency_approver of dependency_approvers) {
-        dependency_approver_promises.push(dependency_approver(active_password_resets.map(active_password_reset => ({ old: active_password_reset, new: null })), user, session))
-    }
-    const dependency_approver_results = await Promise.all(dependency_approver_promises)
-
-    const unapproved = dependency_approver_results.find(dependency_approver_result => !dependency_approver_result.approved)
-    if (unapproved) {
-        if (!parent_session) {
-            if (session.inTransaction()) await session.abortTransaction()
-            await session.endSession()
-        }
-        return {
-            code: 403,
-            data: unapproved.reason
-        }
-    }
-
-    // 7. Check collection integrity
-    // Nothing needs to be checked.
-
-    // 8. Remove documents
-    const ids_to_delete = active_password_resets.map((active_password_reset) => active_password_reset._id)
-    await Active_Password_Reset_Model.deleteMany({
-        _id: { $in: ids_to_delete },
-    }, {
-        session: session
-    })
-
-    // 9. Update dependents
-    // There are no dependents of Active_Password_Reset.
-
-    // 10. Commit transaction and end session
-    if (!parent_session) {
-        if (session.inTransaction()) await session.commitTransaction()
-        await session.endSession()
-    }
-
-    // 11. Reply
+    // 5. Reply
     return {
-        code: 200,
-        data: undefined,
+        code: 204
     }
 }
 
